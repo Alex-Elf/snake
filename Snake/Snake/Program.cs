2a4a15172bf89f82915f61ae304bbebfb12c64ee
Alex <popov.alex.elf@gmail.com>
2016-05-23 23:23:09 +0300

    New class point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,16 +10,10 @@ namespace Snake
     {
         static void Main()
         {
-            int x1 = 1, y1= 3;
-            char char1 = '*';
-            Draw(x1, y1, char1);
+
+            //Draw(x1, y1, char1);
 
             Console.Read();
-        }
-        static void Draw(int x, int y, char c)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(c);
         }
     }
 }
